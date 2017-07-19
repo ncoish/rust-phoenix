932db07bbff175c142058d29912ae9ffabdbb7b9
Nick Coish <ncoish@nymi.com>
2017-07-19 14:58:43 -0400

    Add State and Event Enums Update to rust-websocket library

--- src/channel.rs
+++ src/channel.rs
@@ -1,7 +1,44 @@
-extern crate ws;
+enum ChannelStates {
+    Closed,
+    Errored,
+    Joined,
+    Joining,
+    Leaving,
+}
 
-use ws::{connect, CloseCode};
+impl ChannelStates {
+    fn value(&self) -> &str {
+        match *self {
+            ChannelStates::Closed  => "closed",
+            ChannelStates::Errored => "errored",
+            ChannelStates::Joined  => "joined",
+            ChannelStates::Joining => "joining",
+            ChannelStates::Leaving => "leaving",
+        }
+    }
+}
 
-mod channel {
+enum ChannelEvents {
+    Close,
+    Error,
+    Join,
+    Reply,
+    Leave,
+}
+
+impl ChannelEvents {
+    fn value(&self) -> &str {
+        match *self {
+            ChannelEvents::Close => "phx_close",
+            ChannelEvents::Error => "phx_error",
+            ChannelEvents::Join  => "phx_join",
+            ChannelEvents::Reply => "phx_reply",
+            ChannelEvents::Leave => "phx_leave",
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Channel {
 
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,12 @@
+#![cfg_attr(all(test, feature = "nightly"), feature(test))]
 
+//! Phoenix is a Phoenix client library written in Rust.
+//!
+//! 
+
+#[cfg(all(feature = "nightly", test))]
+extern crate test;
 
 mod channel;
-
+mod socket;
+mod long_poller;
