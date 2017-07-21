7c37cf33200c81fb6fe02387eed695cf49be535e
Nick Coish <ncoish@nymi.com>
2017-07-21 15:00:38 -0400

    temp

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#![allow(warnings)]
 #![cfg_attr(all(test, feature = "nightly"), feature(test))]
 
 //! Phoenix is a Phoenix client library written in Rust.
@@ -6,6 +7,7 @@
 
 #[cfg(all(feature = "nightly", test))]
 extern crate test;
+extern crate websocket;
 
 pub mod channel;
 pub mod socket;
@@ -13,12 +15,21 @@ pub mod socket;
 #[cfg(test)]
 mod tests {
     use super::socket;
+    // #[test]
+    // fn set_callback_test() {
+    //     let mut sock = socket::Socket::new("Hello");
+    //     sock.set_callback_open(|| println!("Oh hello!"));
+    //     sock.process_events();
+    //     sock.set_callback_open(|| println!("Mello yellow"));
+    //     sock.process_events();
+    // }
+
     #[test]
-    fn set_callback_test() {
-        let mut sock = socket::Socket::new("Hello");
-        sock.set_callback_open(|| println!("Oh hello!"));
-        sock.process_events();
-        sock.set_callback_open(|| println!("Mello yellow"));
-        sock.process_events();
+    fn can_construct_and_use_socket() {
+        let mut socket_builder = socket::SocketBuilder::new(String::from("url"))
+        .add_on_open(|| println!{"Hello there!"})
+        .add_on_message(|message| println!("{}", message));
+        let mut socket = socket_builder.finish();
+        socket.process_events();
     }
 }
--- src/socket.rs
+++ src/socket.rs
@@ -1,5 +1,8 @@
 use std;
 use std::collections::HashMap;
+use std::net::TcpStream;
+use websocket::ClientBuilder;
+use websocket::client::async::Client;
 
 enum SocketState {
     Connecting,
@@ -8,28 +11,25 @@ enum SocketState {
     Closed,
 }
 
-// Probably want to implement builder pattern here
+// pub enum Transport {
+//     //Longpoll(Option<i32>),
+//     Websocket(Client<TcpStream>),
+// }
+
+// TODO: Probably want to implement builder pattern here
+// TODO: Callbacks attributes should probably be list of callbacks
+//       to conform to javascript client library.
 pub struct Socket {
-    state_change_open:    Option<Box<FnMut()>>,
-    state_change_close:   Option<Box<FnMut()>>,
-    state_change_error:   Option<Box<FnMut()>>,
-    state_change_message: Option<Box<FnMut()>>,
+    endpoint:               String,
+    //transport:              Transport,
+    connection:             Option<Client<TcpStream>>,
+    state_change_open:      Option<Box<FnMut()>>,
+    state_change_close:     Option<Box<FnMut(String)>>,
+    state_change_error:     Option<Box<FnMut(String)>>,
+    state_change_message:   Option<Box<FnMut(String)>>,
 }
 
 impl Socket {
-    pub fn new(endpoint: &str) -> Self {
-        Socket {
-            state_change_open:    None,
-            state_change_close:   None,
-            state_change_error:   None,
-            state_change_message: None,
-        }
-    }
-
-    // pub fn new(endpoint: &str, opts: Option<HashMap<String, String>>) -> Self {
-
-    // }
-
     pub fn set_callback_open<CB: 'static + FnMut()>(&mut self, c: CB) {
         self.state_change_open = Some(Box::new(c));
     }
@@ -37,6 +37,69 @@ impl Socket {
     pub fn process_events(&mut self) {
         if let Some(ref mut func) = self.state_change_open {
             (func)()
+        }
+        else {
+            println!("it failed!")
+        }
+    }
+
+    pub fn connect() {
+
+    }
+}
+
+#[allow(dead_code)]
+pub struct SocketBuilder {
+    endpoint:               String,
+    //transport:              Transport,
+    state_change_open:      Option<Box<FnMut()>>,
+    state_change_close:     Option<Box<FnMut(String)>>,
+    state_change_error:     Option<Box<FnMut(String)>>,
+    state_change_message:   Option<Box<FnMut(String)>>,
+}
+
+#[allow(dead_code)]
+impl SocketBuilder {
+    pub fn new(endpoint: String) -> SocketBuilder {
+        SocketBuilder {
+            endpoint:               endpoint,
+            //transport:              transport,
+            state_change_open:      None,
+            state_change_close:     None,
+            state_change_error:     None,
+            state_change_message:   None,
+        }
+    }
+
+    pub fn add_on_open<'a, CB: 'static + FnMut()>(&'a mut self, c: CB) -> &'a mut Self {
+        self.state_change_open = Some(Box::new(c));
+        self
+    }
+
+    pub fn add_on_close<'a, CB: 'static + FnMut(String)>(&'a mut self, c: CB) -> &'a mut Self {
+        self.state_change_close = Some(Box::new(c));
+        self
+    }
+
+    pub fn add_on_error<'a, CB: 'static + FnMut(String)>(&'a mut self, c: CB) -> &'a mut Self {
+        self.state_change_error = Some(Box::new(c));
+        self
+    }
+
+    pub fn add_on_message<'a, CB: 'static + FnMut(String)>(&'a mut self, c: CB) -> &'a mut Self {
+        self.state_change_message = Some(Box::new(c));
+        self
+    }
+
+    pub fn finish(self) -> Socket {
+        Socket {
+            endpoint:               self.endpoint,
+            //transport:              self.transport,
+            connection:             None,
+            state_change_open:      self.state_change_open,
+            state_change_close:     self.state_change_close,
+            state_change_error:     self.state_change_error,
+            state_change_message:   self.state_change_message,
         }
     }
 }
