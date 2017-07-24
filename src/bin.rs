fc459ae833efb62e31dee3cbd4a25ed0d7a409f9
Nick Coish <ncoish@nymi.com>
2017-07-24 19:18:15 -0400

    Testing app set up to properly connect to websocket

--- src/bin.rs
+++ src/bin.rs
@@ -5,15 +5,19 @@ use std::{thread, time};
 use phoenix::socket::SocketBuilder;
 
 fn main() {
-    let socket_builder = SocketBuilder::new(String::from("ws://127.0.0.1:2794"))
+    let socket_builder = SocketBuilder::new(String::from("ws://127.0.0.1:4000/socket/websocket"))
         .add_on_open(|| println!("We're open for business!"))
         .add_on_message(|message| println!("{:?}", message));
 
     let mut socket = socket_builder.finish();
-    //socket.connect();
+    match socket.connect() {
+        Ok(()) => (),
+        Err(e) => println!("Error: {:?}", e),
+    }
     println!("We made it here!");
+    socket.send(String::from("{ \"topic\": \"lobby\", \"event\": \"phx_join\", \"payload\": {}, \"ref\": 1 }"));
     loop {
         thread::sleep(time::Duration::from_millis(1000));
-        socket.send(String::from("Hello there!"));
+        //socket.send(String::from("{ \"topic\": \"lobby\", \"event\": \"phx_join\", \"payload\": {}, \"ref\": 1 }"));
     }
 }
